07084dade1b3fa2a66a607b8ebffb97ea2cd716a
priyankayuvrajpatil <pypatil17@gmail.com>
2017-06-27 14:42:27 +0530

    Add files via upload

---
+++ ApplicationConstant.java
@@ -0,0 +1,20 @@
+package com.seleniumproject;
+
+
+
+public class ApplicationConstant {
+	
+	public static final String search_key = "Sony TV";
+	public static final String application_url= "http://www.ebay.com";
+	public static final String searchbox_id= "gh-ac";
+	public static final String searchbutton_id="gh-btn";
+	public static final String PRODUCT_NOT_CONTAINS_SERCHED_KEY = "Product not contain serached key";
+	public static final String SCREENSHOT_IMAGE_PATH ="C:/Users/priyanka_patil11/selenium_doc";
+	
+	public static final String linktext ="https://www.ebay.com/sch/i.html?Screen%2520Size=50%2522%2520%252D%252060%2522&_sop=12&_sacat=11071&_nkw=sony tv&_dcat=11071";
+	public static final String symbol="US $";
+	public static final String CartHeader = "Your eBay Shopping Cart";
+	
+	
+	
+	}
