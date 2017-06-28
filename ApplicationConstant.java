a88659f00584b8f37e274d6c31ae393f3fccf43b
priyankayuvrajpatil <pypatil17@gmail.com>
2017-06-28 15:09:22 +0530

    Add files via upload

--- ApplicationConstant.java
+++ ApplicationConstant.java
@@ -14,6 +14,7 @@ public class ApplicationConstant {
 	public static final String linktext ="https://www.ebay.com/sch/i.html?Screen%2520Size=50%2522%2520%252D%252060%2522&_sop=12&_sacat=11071&_nkw=sony tv&_dcat=11071";
 	public static final String symbol="US $";
 	public static final String CartHeader = "Your eBay Shopping Cart";
+	public static final String checkoutScreenPage="Guest checkout";
 	
 	
 	
